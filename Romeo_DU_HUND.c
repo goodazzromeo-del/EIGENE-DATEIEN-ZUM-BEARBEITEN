eeddb258b82115f9112e4143058bad8cf7ac0c73
GoodazzRomeo <Goodazzromeo@gmail.com>
2026-07-29 16:21:27 +0200

    bumm

--- Romeo_DU_HUND.c
+++ Romeo_DU_HUND.c
@@ -7,9 +7,6 @@ int main()
     MessageBoxW(NULL, L"ROMEO DU STÜCK SCHEIßE!!!", L"ROMEO DU PISSER ❤️❤️", MB_OK | MB_ICONINFORMATION);
     
     getchar();
-    /*einfach es besser machen und schauen ob es auf GITHUB LANDET ENDLCIH MAL*/
-    
     
     return 0;
-
 }
