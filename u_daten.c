44dc16ebf8ff966d35701cf580671b1fc03867e2
GoodazzRomeo <Goodazzromeo@gmail.com>
2026-08-05 21:40:13 +0200

    Der Text endlich komplett alleine Geschrieben, Fehler Bereinigt selber nochmal. ChatGpt aber nachgefragt einmal da nicht jeder Buchstabe einfach hinter % stehen kann und der einzige Unterschied zwischen, Double, Float und Integer nicht nur Buchstaben Anzahl is sondern genaue Buchstaben. Und einmal halt ein & vergessen gehabt bei PreisZweiterArtikel.

--- u_daten.c
+++ u_daten.c
@@ -4,7 +4,7 @@ int main()
 {
     /*Die Variablen werden hier definiert*/
     int ErsterArtikelAnzahl, ZweiterArtikelAnzahl;
-    double PreisErsterArtikel, PreisZweiterArtikel, gesamtPreisBeide, gesamtpreisRabatt;
+    double PreisErsterArtikel, PreisZweiterArtikel, gesamtPreisBeide, gesamtpreisRabatt, gesamtpreisRabattDifferenz;
 
 
     setlocale(LC_ALL, "German");
@@ -12,7 +12,7 @@ int main()
     /*Der Erste Artikel wird abgefragt und der Preis wird abgefragt*/
 
     printf("Gib bitte die Anzahl des Ersten Artikels ein:\n");
-    wscanf(L"%e", &ErsterArtikelAnzahl);
+    scanf("%d", &ErsterArtikelAnzahl);
     
     wprintf(L"Bitte gib den Preis deines Ersten Artikel pro Stück ein:\n");
     wscanf(L"%le", &PreisErsterArtikel);
@@ -20,22 +20,23 @@ int main()
     /*Der zweite Artikel wird im Preis abgefragt und der Anzahl*/
 
     printf("Bitte gib mir die Anzahl des zweiten Artikels an:\n");
-    wscanf(L"%a", &ZweiterArtikelAnzahl);
+    scanf("%d", &ZweiterArtikelAnzahl);
 
     wprintf(L"Bitte gib mir den Preis des zweiten Artikels an:\n");
-    wscanf(L"%za", PreisZweiterArtikel);
+    wscanf(L"%lf", &PreisZweiterArtikel);
 
     /*Hier wird alles zusammen gerechnet als auch der Rabattpreis Berechnet*/
     
     gesamtPreisBeide = (ErsterArtikelAnzahl * PreisErsterArtikel) + (PreisZweiterArtikel * ZweiterArtikelAnzahl); 
 
+    gesamtpreisRabattDifferenz =  gesamtPreisBeide / 5;
 
-    gesamtpreisRabatt =  5 /  gesamtPreisBeide;
+    gesamtpreisRabatt = gesamtPreisBeide - gesamtpreisRabattDifferenz;
 
     /*Hier werden die Preise beide Ausgegben und dem Einkäufer gesagt*/
 
-    wprintf(L"Die Summe ohne Rabatt aller Artikel ist: %.2lg\n", gesamtPreisBeide);
-    wprintf(L"Durch den 20 Prozent Rabatt kostet der Einkauf aber nur: %.2lg\n", gesamtpreisRabatt);
+    wprintf(L"Die Summe ohne Rabatt aller Artikel ist: %.2lf\n", gesamtPreisBeide);
+    wprintf(L"Durch den 20 Prozent Rabatt kostet der Einkauf aber nur: %.2lf\n", gesamtpreisRabatt);
 
     getchar();
 
