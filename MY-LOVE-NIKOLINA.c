d9760b1b5ea5934192971e1cabdb68013bb2614c
GoodazzRomeo <goodazzromeo@gmail.com>
2026-08-28 21:32:50 +0200

    Noch nicht das wissen um das Programm umzusetzen aber ich Liebe dich Nikolina

--- MY-LOVE-NIKOLINA.c
+++ MY-LOVE-NIKOLINA.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <locale.h>
-int main ()
+int main()
 {
     setlocale(LC_ALL, "German");
-    wscanf(L"")
+
+    wprintf(L"Liebst du mich wirklich?\n");
+
+    char Eingabe;
+
+    wscanf(L"%c", &Eingabe);
+
+
+
+    if(Eingabe == "Ja" )
+    {
+
+        wprintf(L"Ich liebe dich viel mehr, über alles sogar");
+    }
+        else
+    {
+        
+        wprintf(L"Tja dachte ich mir ich liebe dich mehr.");
+    }
+
+    return 0;
 }
